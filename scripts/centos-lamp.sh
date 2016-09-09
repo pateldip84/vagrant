e1e9aad034855614cf125f99198b9ab468451bcb
Dipan Patel <dipatel@softlayer.com>
2016-09-09 13:27:12 -0500

    added lamp script

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -28,4 +28,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 #DOWNLOAD starter content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/pateldip84/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/pateldip84/vagrant/master/files/info.php
+
 service httpd restart
